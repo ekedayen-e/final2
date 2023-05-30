e2980010b6bae3a5d7cb836532fc2a248c0535cc
erubae <rombuski@gmail.com>
2023-05-30 13:15:44 +0100

    fun

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser= require('body-parser')
+const mongoose = require('mongoose')
+const routes = require("./routes/api")
+require('dotenv').config({path:"./config.env"})
+let app = express()
+const port = process.env.PORT || 3001
+
+mongoose
+    .connect(process.env.DB, { useNewUrlParser: true })
+    .then(() => console.log("Database connected successfully"))
+    .catch((err) => console.log(err));
+
+mongoose.Promise = global.Promise
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use("/api/entries", routes)
+
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
+   }
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    next();
+  });
+
+app.listen(port, () =>  {
+    console.log(`Server running on port ${port}`)
+})
